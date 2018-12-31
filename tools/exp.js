430d77d7eae7bb14f114e777fd97e3db803e5a63
ligand <fry@Mac.local>
2018-12-31 15:44:22 +0800

    从graphics 到svg到工具

---
+++ tools/exp.js
@@ -0,0 +1,38 @@
+// 实验性质的代码。使用者请忽略。
+
+// 探索svg动画中的，animation-duration 和 storke-dashoffset 与 笔画路径长度的关系。
+
+path00 = {
+    length: 321.08990478515625,
+    animationDuration: 0.7195638020833334,
+    dashoffset: 577
+}
+
+path01 = {
+    length: 330.7146301269531,
+    animationDuration: 0.7277018229166666,
+    dashoffset: 587
+}
+
+path10 = {
+    length: 403.7048645019531,
+    animationDuration: 0.787109375,
+    dashoffset: 660
+}
+
+path11 = {
+    length: 465.853515625,
+    animationDuration: 0.8375651041666666,
+    dashoffset: 722
+}
+
+path20 = {
+    length:914.2689819335938,
+    animationDuration: 1.2021484375,
+    dashoffset: 1170
+}
+path21 = {
+    length: 257.5745544433594,
+    animationDuration: 0.6682942708333334,
+    dashoffset: 514
+}
